5f9ede13b58469df0857e12f85ee1b30ffd7a9ab
hchoi3 <hyunmin.choi@axleinfo.com>
2023-07-27 17:58:13 -0400

    deleted alternative

--- aws/cloudwatch-trainee/main.tf
+++ aws/cloudwatch-trainee/main.tf
@@ -13,8 +13,6 @@ module "sqs_queue" {
 module "sns_topic_subscription"{
   source = "../../terraform-modules/aws/sns/sns_topic_subscription"
   sns_topic_subscription_topic_arn = module.sns_topic.sns_topic_arn
-  #"arn:aws:sns:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:${var.sns_topic_name}"
   sns_topic_subscription_protocol = "sqs"
   sns_topic_subscription_endpoint = module.sqs_queue.base_queue_arn
-  #"arn:aws:sqs:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:${var.queue_name}"
 }
