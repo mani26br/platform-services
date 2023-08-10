a490da9df6a69252aa598a1bdbc6f9f1bfab2d4f
hchoi3 <hyunmin.choi@axleinfo.com>
2023-08-10 12:35:09 -0400

    added index of log group data source

--- aws/test/main.tf
+++ aws/test/main.tf
@@ -1,9 +1,15 @@
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
+data "aws_vpcs" "current" {}
 data "aws_cloudwatch_log_groups" "example" {
-  log_group_name_prefix = "/aws/vpcflowlogs/"
+  #log_group_name_prefix = "/aws/vpcflowlogs/"
+  log_group_name_prefix = "/aws/eks/"
 }
 
 output "test" {
-  value = data.aws_cloudwatch_log_groups.example.log_group_names
+  value = tolist(data.aws_cloudwatch_log_groups.example.log_group_names)[0]
+}
+
+output "vpc" {
+  value = data.aws_vpcs.current.ids
 }
