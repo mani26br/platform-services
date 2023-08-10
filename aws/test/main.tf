1ed2bdbbda6066c8e015d1ddd5176c9a92dfb0ec
hchoi3 <hyunmin.choi@axleinfo.com>
2023-08-10 12:15:32 -0400

    data source for log groups

--- aws/test/main.tf
+++ aws/test/main.tf
@@ -1,26 +1,9 @@
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
-data "aws_vpc" "current" {
-  tags = {
-    Department = "DevSecOps Associate"
-    Name = "DevSecOps-vpc"
-  }
+data "aws_cloudwatch_log_groups" "example" {
+  log_group_name_prefix = "/aws/vpcflowlogs/"
 }
 
-resource "aws_route53_resolver_query_log_config" "example" {
-  name            = "CW-query-logs"
-  destination_arn = "arn:aws:logs:us-east-1:594244466763:log-group:/aws/route53/cw-log-query-logs"
-
-  tags = {
-    Environment = "Prod"
-  }
+output "test" {
+  value = data.aws_cloudwatch_log_groups.example.log_group_names
 }
-
-data "aws_route53_zone" "all_zones" {
-  name = "axle-interns.com"
-}
-
-data "aws_route53_zone" "all_zones2" {
-  name = "ci.intern.aws.labshare.org"
-}
-
